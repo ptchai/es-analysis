a76bdc8f23ae8f835eea8d4ffdbf67b966be419a
Peter Chai <pt.chai@hotmail.com>
2026-04-02 18:31:56 -0700

    clean interface

--- src/main/java/com/es/analysis/services/NumberService.java
+++ src/main/java/com/es/analysis/services/NumberService.java
@@ -5,5 +5,5 @@ import java.util.List;
 public interface NumberService {
     int[] extractNumbers(List<int[]> generatedArrays, int min, int max);
     int getLargestPrimeNumber(int[] numbers);
-    void perform() throws InterruptedException; 
+    void perform();
 }
